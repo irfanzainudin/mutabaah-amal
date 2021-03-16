1ccbc05d0e784f2f1acf49a1af6274ac1605ebc3
irfanzainudin <m.irfan.zain@gmail.com>
2021-03-16 23:45:55 +1100

    started work on firebase integration

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React from 'react';
 import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+// import admin from 'firebase-admin';
 
+// pages
 import Home from './pages/Home';
 import Chatroom from './pages/Chatroom';
+import Profile from './pages/Profile';
+
+// stylesheets
 import './App.css';
+
+// api config
+// import firebaseConfig from './api/firebase';
+
+// admin.initializeApp(firebaseConfig);
+// const db = admin.firestore();
 
 function App() {
   return (
@@ -13,12 +24,13 @@ function App() {
       <div className="Navbar">
         <div className="Navbar-header">
           <Link to="/"><h3 className="Home-link">Mutabaah Amal</h3></Link>
-          <button><FontAwesomeIcon icon={faUserCircle} className="User-icon" /></button>
+          <button><Link to="/profile"><FontAwesomeIcon icon={faUserCircle} className="User-icon" /></Link></button>
         </div>
       </div>
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route path="/chatroom" component={Chatroom}/>
+        <Route path="/profile" component={Profile}/>
       </Switch>
     </Router>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheckSquare, faPlusCircle, faStickyNote } from '@fortawesome/free-solid-svg-icons';
+import { faCheckSquare, faPlusCircle, faCommentDots } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -45,13 +45,13 @@ var placeholder_amalan = [
 function Home() {
   const [amalan, setAmalan] = useState(placeholder_amalan);
 
-  function checkDone(amal_done) {
-    const updated_amalan = amalan.filter(item => item.amal !== amal_done);
+  function checkDone(amal_id) {
+    const updated_amalan = amalan.filter(item => item.id !== amal_id);
     setAmalan(updated_amalan);
   }
 
   function addAmal() {
-    const new_amal = { amal: 'Istighfar 100 kali', frequency: '1/week' };
+    const new_amal = { id: uuidv4(), amal: 'Istighfar 100 kali', frequency: '1/week' };
     const updated_amalan = amalan.concat(new_amal);
     setAmalan(updated_amalan);
   }
@@ -66,13 +66,13 @@ function Home() {
         </button>
       </div>
       {
-        amalan.map((item) => {
+        amalan.length == 0 ? <h3 className="center">You're all caught up, alhamdulillah!</h3> : amalan.map((item) => {
           return (
             <div className="Amal-container" key={uuidv4()}>
               <div className="Amal-card">
-                <Link to="/chatroom"><button><FontAwesomeIcon icon={faStickyNote} /></button></Link>
+                <Link to="/chatroom"><button><FontAwesomeIcon icon={faCommentDots} /></button></Link>
                 <h3 className="Amal-title">{item.amal}</h3>
-                <button onClick={() => checkDone(item.amal)}><FontAwesomeIcon icon={faCheckSquare} /></button>
+                <button onClick={() => checkDone(item.id)}><FontAwesomeIcon icon={faCheckSquare} /></button>
               </div>
               <div className="Amal-note">
                 <p>{item.frequency}</p>
